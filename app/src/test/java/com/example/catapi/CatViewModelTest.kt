8825ae8931162b8008cf6e585de4577db333a17c
Kwounsu <sb00lgs@gmail.com>
2020-06-29 10:23:15 -0700

    mockito TestObserver

--- app/src/test/java/com/example/catapi/CatViewModelTest.kt
+++ app/src/test/java/com/example/catapi/CatViewModelTest.kt
@@ -8,6 +8,11 @@ import com.example.catapi.model.Weight
 import com.example.catapi.viewmodel.CatViewModel
 import io.reactivex.Maybe
 import io.reactivex.Observable
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.observers.TestObserver
+import io.reactivex.schedulers.Schedulers
+import org.hamcrest.CoreMatchers.`is`
+import org.hamcrest.MatcherAssert.assertThat
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
@@ -39,17 +44,23 @@ class CatViewModelTest {
         val list = listOf<String>()
         val images = listOf(Images(list, "coj","https://cdn2.thecatapi.com/images/coj.jpg",427,640))
         Mockito.`when`(this.apiService.getImage()).thenReturn(Observable.just(images))
-//        Mockito.`when`(this.apiService.getImage()).thenAnswer {
-//            return@thenAnswer Maybe.just(ArgumentMatchers.anyList<Images>())
-//        }
+//        Mockito.`when`(this.apiService.getImage()).thenAnswer { return@thenAnswer Maybe.just(ArgumentMatchers.anyList<Images>()) }
+
+        val testObserver = TestObserver<List<Images>>()
+        apiService.getImage().subscribe(testObserver)
+        testObserver.assertComplete()
+        testObserver.assertNoErrors()
+        testObserver.assertValueCount(1)
+
+        val listResult = testObserver.values()[0]
+        assertThat(listResult.size, `is`(1))
+        assertThat(listResult[0].id, `is`("coj"))
     }
 
     @Test
     fun getBreeds_positiveResponse() {
         val breeds = listOf(Breeds(Weight("1","2"),"1","1","1","1","1","1","1","1","1","1","1",1,1,"1",1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,"1",1))
         Mockito.`when`(this.apiService.getBreeds()).thenReturn(Observable.just(breeds))
-//        Mockito.`when`(this.apiService.getBreeds()).thenAnswer {
-//            return@thenAnswer Maybe.just(ArgumentMatchers.anyList<Breeds>())
-//        }
+//        Mockito.`when`(this.apiService.getBreeds()).thenAnswer { return@thenAnswer Maybe.just(ArgumentMatchers.anyList<Breeds>()) }
     }
 }
